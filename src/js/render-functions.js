615e24f7c1069dbeb6e9b2800f57dc782b2df4b2
LAG0MM <166709774+LAG0MM@users.noreply.github.com>
2026-02-17 17:18:55 +0100

    Update render-functions.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,7 +1,17 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
 
+const lightbox = new SimpleLightbox('.gallery a', {
+    
+  nav: true,       
+  close: true,     
+  captions: true,
+  captionsData: 'alt', 
+  captionDelay: 250  
+});
 
 export  function createGallery(images) { 
      const markup = images
@@ -19,6 +29,8 @@ export  function createGallery(images) {
       </div>
      `).join('');
      gallery.insertAdjacentHTML('beforeend', markup)
+
+      lightbox.refresh();
     };
 
 export function clearGallery() {
